6b318f9aab3a6cc71e0706f1a38f50d24556305b
magazinebrute <reh3090reh7847>
2017-03-24 16:46:23 -0500

    Added Initial Project.

---
+++ ActualInstaller2NISTInstaller/ActualInstaller2NISTInstaller/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ActualInstaller2NISTInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1_aip_script = new System.Windows.Forms.TextBox();
+            this.openFileDialog_Actual = new System.Windows.Forms.OpenFileDialog();
+            this.button_browse_aip = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_nsi_file = new System.Windows.Forms.TextBox();
+            this.button_create = new System.Windows.Forms.Button();
+            this.labelstatus = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxLicenseSection = new System.Windows.Forms.CheckBox();
+            this.checkBoxuninstall_section = new System.Windows.Forms.CheckBox();
+            this.textBox_optional_program_folder = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1_aip_script
+            // 
+            this.textBox1_aip_script.Location = new System.Drawing.Point(15, 25);
+            this.textBox1_aip_script.Name = "textBox1_aip_script";
+            this.textBox1_aip_script.Size = new System.Drawing.Size(909, 20);
+            this.textBox1_aip_script.TabIndex = 0;
+            // 
+            // openFileDialog_Actual
+            // 
+            this.openFileDialog_Actual.DefaultExt = "aip";
+            this.openFileDialog_Actual.Filter = "Actual Installer Scripts|*.aip|All files|*.*";
+            this.openFileDialog_Actual.ReadOnlyChecked = true;
+            this.openFileDialog_Actual.Title = "Open Actual Installer aip Script File:";
+            // 
+            // button_browse_aip
+            // 
+            this.button_browse_aip.AutoEllipsis = true;
+            this.button_browse_aip.Location = new System.Drawing.Point(930, 25);
+            this.button_browse_aip.Name = "button_browse_aip";
+            this.button_browse_aip.Size = new System.Drawing.Size(75, 23);
+            this.button_browse_aip.TabIndex = 1;
+            this.button_browse_aip.Text = "Browse";
+            this.button_browse_aip.UseVisualStyleBackColor = true;
+            this.button_browse_aip.Click += new System.EventHandler(this.button_browse_aip_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select your Actual Installer .aip script:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(667, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter filename with no extension. The .nsi extension will be applied:  (note: scr" +
+    "ipt will be generated in same directory of actuall installer script.)";
+            // 
+            // textBox_nsi_file
+            // 
+            this.textBox_nsi_file.Location = new System.Drawing.Point(15, 74);
+            this.textBox_nsi_file.Name = "textBox_nsi_file";
+            this.textBox_nsi_file.Size = new System.Drawing.Size(180, 20);
+            this.textBox_nsi_file.TabIndex = 4;
+            this.textBox_nsi_file.TextChanged += new System.EventHandler(this.textBox_nsi_file_TextChanged);
+            // 
+            // button_create
+            // 
+            this.button_create.Enabled = false;
+            this.button_create.Location = new System.Drawing.Point(12, 118);
+            this.button_create.Name = "button_create";
+            this.button_create.Size = new System.Drawing.Size(176, 60);
+            this.button_create.TabIndex = 5;
+            this.button_create.Text = "Create NIST Script";
+            this.button_create.UseVisualStyleBackColor = true;
+            this.button_create.Click += new System.EventHandler(this.button_create_Click);
+            // 
+            // labelstatus
+            // 
+            this.labelstatus.AutoSize = true;
+            this.labelstatus.Location = new System.Drawing.Point(214, 142);
+            this.labelstatus.Name = "labelstatus";
+            this.labelstatus.Size = new System.Drawing.Size(0, 13);
+            this.labelstatus.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_optional_program_folder);
+            this.groupBox1.Controls.Add(this.checkBoxuninstall_section);
+            this.groupBox1.Controls.Add(this.checkBoxLicenseSection);
+            this.groupBox1.Location = new System.Drawing.Point(23, 200);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(959, 84);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Output Options:";
+            // 
+            // checkBoxLicenseSection
+            // 
+            this.checkBoxLicenseSection.AutoSize = true;
+            this.checkBoxLicenseSection.Checked = true;
+            this.checkBoxLicenseSection.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxLicenseSection.Location = new System.Drawing.Point(6, 19);
+            this.checkBoxLicenseSection.Name = "checkBoxLicenseSection";
+            this.checkBoxLicenseSection.Size = new System.Drawing.Size(136, 17);
+            this.checkBoxLicenseSection.TabIndex = 0;
+            this.checkBoxLicenseSection.Text = "Create License Section";
+            this.checkBoxLicenseSection.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxuninstall_section
+            // 
+            this.checkBoxuninstall_section.AutoSize = true;
+            this.checkBoxuninstall_section.Checked = true;
+            this.checkBoxuninstall_section.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxuninstall_section.Location = new System.Drawing.Point(6, 42);
+            this.checkBoxuninstall_section.Name = "checkBoxuninstall_section";
+            this.checkBoxuninstall_section.Size = new System.Drawing.Size(109, 17);
+            this.checkBoxuninstall_section.TabIndex = 1;
+            this.checkBoxuninstall_section.Text = "Create Uninstaller";
+            this.checkBoxuninstall_section.UseVisualStyleBackColor = true;
+            // 
+            // textBox_optional_program_folder
+            // 
+            this.textBox_optional_program_folder.Location = new System.Drawing.Point(371, 17);
+            this.textBox_optional_program_folder.Name = "textBox_optional_program_folder";
+            this.textBox_optional_program_folder.Size = new System.Drawing.Size(156, 20);
+            this.textBox_optional_program_folder.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Optional Program Files Folder to Install into:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 306);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelstatus);
+            this.Controls.Add(this.button_create);
+            this.Controls.Add(this.textBox_nsi_file);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_browse_aip);
+            this.Controls.Add(this.textBox1_aip_script);
+            this.Name = "Form1";
+            this.Text = "Actual Installer  ->  NIST Script Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1_aip_script;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_Actual;
+        private System.Windows.Forms.Button button_browse_aip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_nsi_file;
+        private System.Windows.Forms.Button button_create;
+        private System.Windows.Forms.Label labelstatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxLicenseSection;
+        private System.Windows.Forms.CheckBox checkBoxuninstall_section;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_optional_program_folder;
+    }
+}
+
---
+++ ActualInstaller2NISTInstaller/ActualInstaller2NISTInstaller/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IniParser;
+using IniParser.Model;
+using System.IO;
+
+namespace ActualInstaller2NISTInstaller
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_browse_aip_Click(object sender, EventArgs e)
+        {
+            openFileDialog_Actual.DefaultExt = "aip";
+            DialogResult dr = openFileDialog_Actual.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                textBox1_aip_script.Text = openFileDialog_Actual.FileName.ToString();
+                if (textBox_nsi_file.Text.Length > 0) button_create.Enabled = true;
+            }
+        }
+
+        private void button_create_Click(object sender, EventArgs e)
+        {
+            button_create.Enabled = true;
+            if (textBox_nsi_file.TextLength == 0 || textBox1_aip_script.TextLength == 0)
+            {
+                button_create.Enabled = false;
+                MessageBox.Show("Make sure you have an actual installer script selected and an output file NIST file name with no extension entered.");
+            }
+            else { process(); }
+        }
+
+        private void textBox_nsi_file_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox_nsi_file.TextLength > 0) button_create.Enabled = true;
+        }
+
+        /// <summary>
+        /// convert aip to nsi
+        /// </summary>
+        private void process()
+        {
+            try
+            {
+                //load aip/ini file
+                string _file = textBox1_aip_script.Text.ToString();
+                var parser = new FileIniDataParser();
+                IniData i = parser.ReadFile(_file);
+
+
+                string GUID = i["Setup"]["GUID"];
+                string appname = i["Setup"]["AppName"];
+                string appversion = i["Setup"]["AppVersion"];
+                string appdescription = i["Setup"]["AppDescription"];
+                string companyname = i["Setup"]["CompanyName"];
+                string website = i["Setup"]["WebSite"];
+                string supportlink = i["Setup"]["SupportLink"];
+                string admin = i["Setup"]["Admin"];
+                string setupfilename = i["Setup"]["SetupFileName"];
+                string setupfile = i["Setup"]["SetupFile"];
+                string internet = i["Setup"]["Internet"];
+                string sourcedir = i["Setup"]["SourceDir"];
+                //string sourcedir = @"C:\TEMP\BIN";
+                string launchfilename = i["Setup"]["LaunchFileName"];
+                string launchfilechecked = i["Setup"]["LaunchFileChecked"];
+                string promptuninstall = i["Setup"]["PromptUninstall"];
+                string shortcuts = i["Shortcuts"]["0"];
+                string mainexecutable = i["Setup"]["MainExecutable"];
+
+                //GET LIST OF EXCLUDED FILES
+
+                IniParser.Model.KeyData[] kd = i["FileOptions"].ToArray(); //EXCLUDED FILES
+                var myexcludedfiles = new List<string>();
+                foreach (IniParser.Model.KeyData item in kd) {
+                    if (item.Value.EndsWith("D") || item.Value.Contains(".vshost."))
+                    {
+                        myexcludedfiles.Add(item.Value.Replace("?D", ""));
+                    }
+                }
+
+                //get a structure of all files and subdirs
+                MyFiles myfiles = new MyFiles();
+                List<MyFiles> myworkfiles = new List<MyFiles>();
+                myworkfiles = myfiles.GetDirData(@sourcedir);
+                
+                //remove undesirables
+                myworkfiles.RemoveAll(a => a.filename.Contains(".vshost."));
+                myworkfiles.RemoveAll(a => a.filename.Contains(".aip"));
+
+                for (int z = 0; z < myworkfiles.Count; z++)
+                {
+                    string fullpath = Path.Combine(myworkfiles[z].directoryname, myworkfiles[z].filename);
+                    if (myexcludedfiles.Any(str => str.Equals(fullpath, StringComparison.OrdinalIgnoreCase))) myworkfiles.RemoveAll(a => a.directoryname == myworkfiles[z].directoryname && a.filename == myworkfiles[z].filename);
+                    //remove visual studio files
+                    
+
+                }
+
+                                
+                //now build nsi script
+                string o = "";
+                if (admin == "1") o = "RequestExecutionLevel admin" + Environment.NewLine;
+                o = o + "!include FileFunc.nsh" + Environment.NewLine;
+                o = o + "!insertmacro GetParameters" + Environment.NewLine;
+                o = o + "!insertmacro GetOptions" + Environment.NewLine;
+                o = o + "!include LogicLib.nsh" + Environment.NewLine + Environment.NewLine;
+                o = o + "!define VERSION \"@VERSION\"".Replace("@VERSION",appversion) + Environment.NewLine;
+                o = o + "Name \"@APPNAME v. ${VERSION}\"".Replace("@APPNAME", appname) + Environment.NewLine;
+                o = o + "OutFile \"@APPNAME.exe\"".Replace("@APPNAME", appname.Replace(" ","_")) + Environment.NewLine;
+                if (string.IsNullOrEmpty(textBox_optional_program_folder.Text))
+                {
+                    o = o + "InstallDir \"$PROGRAMFILES\\@APPNAME\"".Replace("@APPNAME",appname) + Environment.NewLine + Environment.NewLine;
+                } else
+                {
+                    o = o + "InstallDir \"$PROGRAMFILES\\@OPTFOLDER\\@APPNAME\"".Replace("@APPNAME", appname).Replace("@OPTFOLDER",textBox_optional_program_folder.Text) + Environment.NewLine + Environment.NewLine;
+                }
+
+                if (checkBoxLicenseSection.Checked)
+                {
+                    o = o + "LicenseData license.txt" + Environment.NewLine;
+                    o = o + "Page license" + Environment.NewLine;
+                }
+                o = o + "Page components" + Environment.NewLine;
+                o = o + "Page directory" + Environment.NewLine;
+                o = o + "Page instfiles" + Environment.NewLine;
+                if (checkBoxuninstall_section.Checked)
+                {
+                    o = o + "UninstPage uninstConfirm" + Environment.NewLine;
+                    o = o + "UninstPage instfiles" + Environment.NewLine;
+                }
+                
+                //start section main
+                o += Environment.NewLine;
+                o += "Section Main" + Environment.NewLine;
+                o += Environment.NewLine;
+                o += "SetShellVarContext all" + Environment.NewLine;
+                //o += "SectionIn RO" + Environment.NewLine;
+                
+                foreach (var item in myworkfiles)
+                {
+                    if (myfiles.currdir == "")
+                    {
+                        myfiles.currdir = item.directoryname;
+                        if (item.directoryname == sourcedir)
+                        {
+                            o += "SetOutPath \"$INSTDIR\"" + Environment.NewLine;
+                        }
+                        else
+                        {
+                            //we have a new segment
+                            string temp = Path.Combine(item.directoryname, item.filename);
+                            o += "SetOutPath \"$INSTDIR\\" + new DirectoryInfo(Path.GetDirectoryName(temp)).Name + "\"" +Environment.NewLine;
+
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (myfiles.currdir != item.directoryname)
+                        {
+                            //we have a new segment
+                            string temp = Path.Combine(item.directoryname, item.filename);
+                            o += "SetOutPath \"$INSTDIR\\" + new DirectoryInfo(Path.GetDirectoryName(temp)).Name + "\"" + Environment.NewLine;
+                            myfiles.currdir = item.directoryname;
+                        }
+                    }
+                    o += "File /nonfatal " + Path.Combine(item.directoryname, item.filename) + Environment.NewLine;
+                }
+                
+                if (checkBoxuninstall_section.Checked && promptuninstall == "1")
+                {
+                    o += Environment.NewLine;
+                    o += "WriteUninstaller \"$INSTDIR\"\\uninstall.exe\"" + Environment.NewLine;
+
+                }
+
+                //add code for writing registry in the future
+                o += "SectionEnd" + Environment.NewLine;
+                o += Environment.NewLine;
+
+                //add start menu
+                o += "Section StartMenu" + Environment.NewLine;
+                o += "SetShellVarContext all" + Environment.NewLine;
+                if (string.IsNullOrEmpty(textBox_optional_program_folder.Text))
+                {
+                    o += "SetOutPath \"$SMPROGRAMS\\@APPNAME\"".Replace("@APPNAME", appname) + Environment.NewLine;
+                }
+                else
+                {
+                    o += "SetOutPath \"$SMPROGRAMS\\@OPTFOLDER\\@APPNAME\"".Replace("@APPNAME", appname).Replace("@OPTFOLDER", textBox_optional_program_folder.Text) + Environment.NewLine;
+                }
+                o += "SetOutPath \"$INSTDIR\"" + Environment.NewLine;
+
+                if (string.IsNullOrEmpty(textBox_optional_program_folder.Text))
+                {
+                    o += "CreateShortCut \"$SMPROGRAMS\\@APPNAME.lnk\" \"$INSTDIR\\@ME".Replace("@APPNAME", appname).Replace("@ME", mainexecutable.Replace("<InstallDir>\\", "")) + "\"" + Environment.NewLine;
+                    
+                }
+                else
+                {
+                    o += "CreateShortCut \"$SMPROGRAMS\\@OPTFOLDER\\@APPNAME.lnk\" \"$INSTDIR\\@ME".Replace("@APPNAME", appname).Replace("@OPTFOLDER", textBox_optional_program_folder.Text).Replace("@ME",mainexecutable.Replace("<InstallDir>\\","")) + "\"" + Environment.NewLine;
+                }
+                o += "SectionEnd" + Environment.NewLine;
+                
+
+                //CreateShortCut "$SMPROGRAMS\CBM\Kiosk\CBM Kiosk.lnk" "$INSTDIR\CBMKiosk.exe"
+                //<CommonDesktop>*?AgencyServer*?<InstallDir>\ie.bat*?AgencyServer AccessPoint*?*?<InstallDir>*?<InstallDir>\AgencyServer.exe*?0*?Normal
+
+                //now write script
+                string writepath = Path.Combine(sourcedir, textBox_nsi_file.Text + ".nsi");
+                if (File.Exists(@writepath)) File.Delete(@writepath);
+                File.AppendAllText(@writepath, o);
+                MessageBox.Show(@writepath + " Created.");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    public class MyFiles
+    {
+        public string currdir { get; set; }
+        public string directoryname {get;set;}
+        public string filename {get;set;}
+        public MyFiles() { currdir = ""; }
+        public List<MyFiles> GetDirData(string filepath)
+        {
+            string oripath = filepath;
+            List<MyFiles> myfiles = new List<MyFiles>();
+            string[] mydirs = Directory.GetDirectories(filepath);
+
+            //load up root files
+            string[] _files = Directory.GetFiles(@filepath);
+            foreach (var item in _files)
+	        {
+		        myfiles.Add( new MyFiles { directoryname= filepath, filename=Path.GetFileName(item)});
+	        }
+
+            //now process subdirs
+            foreach (var dir in mydirs)
+	        {
+                
+                _files = Directory.GetFiles(dir);
+                foreach (var item in _files)
+                {
+                    myfiles.Add( new MyFiles { directoryname= dir, filename=Path.GetFileName(item)});    
+                }
+	        }
+
+            return myfiles;
+
+        }
+    }
+}
